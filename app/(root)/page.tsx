d5da82756935407bc0445f2a69bf0c422b2052a2
Walker Schrumpf <walker.schrumpf@beast-code.com>
2024-09-07 14:54:05 -0500

    Home page changes implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <div className='home-content'>
         <header className='home-header'>
           <HeaderBox type="greeting" title="Welcome" user={loggedIn.firstName || "Guest"} subtext="Access and manage your account and transactions"/>
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
         </header>
       </div>
     </section>
